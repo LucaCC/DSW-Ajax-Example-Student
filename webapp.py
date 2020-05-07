4b4bcf5c2b317a1228864228b426bc195dd4a922
Luca <lucaccorey@gmail.com>
2020-05-07 08:59:43 -0700

    Change name of function

--- webapp.py
+++ webapp.py
@@ -10,7 +10,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/ajaxText')
-def home():
+def ajaxChange():
     return Markup("<h2>This is a new title</h2> <p>This is a new pargraph</p>")
 
 if __name__ == '__main__':
