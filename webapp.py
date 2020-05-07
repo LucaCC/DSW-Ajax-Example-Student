9de06ee79b37af2909da6405c48353670a85bfbf
Luca <lucaccorey@gmail.com>
2020-05-07 08:51:47 -0700

    Markup to change h2 and p

--- webapp.py
+++ webapp.py
@@ -9,5 +9,9 @@ app.debug = True #Change this to False for production
 def home():
     return render_template('home.html')
 
+@app.route('/ajaxText')
+def home():
+    return Markup("<h2>This is a new title</h2> <p>This is a new pargraph</p>")
+
 if __name__ == '__main__':
     app.run()
